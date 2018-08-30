25551aefcac7f700bc2451cc83ac08911c371f01
Devastus <jalopeno@gmail.com>
2018-08-31 01:34:14 +0300

    debugging

--- reactornet/reactornet_server.lua
+++ reactornet/reactornet_server.lua
@@ -131,6 +131,8 @@ local function setupServer()
             os.sleep(2)
         end
     end
+    print("\nid_postfix: "..id_postfix)
+    print("\n"..__SERVER_TYPES[settings.server_type])
     settings.network_id = "RNet_" .. __SERVER_TYPES[settings.server_type] .. "_" .. id_postfix
 
     termUI.clear()
